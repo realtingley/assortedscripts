2702cc9aa488063d84418c252e43071e4c3e8e01
stingley <stingley@cogitocorp.com>
2020-09-28 18:46:33 -0400

    changed script to sort files according to which component the files came from so that engineering can view reports more easily. everything working except for untarring oreca and retarring everything. need to figure out how to untar oreca without it trying to write to the OS and how to remove bash scripts from head of java executables

--- RPM2JAR.sh
+++ RPM2JAR.sh
@@ -1,6 +1,41 @@
 #!/usr/bin/env bash
 
-set -x
+function sortcode {
+  c=$(find $1 -type f \( -name "*.a" -o -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.so" -o -name "*.la" \))
+  java=$(find $1 -type f -name "*.jar" -o -name "*.war" -o -name "*.ear")
+  js=$(find $1 -type f -name "*.js")
+  pl=$(find $1 -type f \( -name "*.pl" -o -name "*.pm" -o -name "*.plx" -o -name "*.pl5" -o -name "*.cgi" \))
+  pysource=$(find $1 -type f -name "*.py")
+  pyall=$(find $1 -type f \( -name "*.py" -o -name "*.htm" -o -name "*.html" \))
+  
+  for x in $java; do
+      cp $x ./java/
+  done
+  for x in $c; do
+      cp $x ./c/
+  done
+  for x in $js; do
+      cp $x ./js/
+  done
+  for x in $pl; do
+      cp $x ./pl/
+  done
+  for x in $pysource; do
+      cp $x ./py/
+  done
+}
+
+function repackcode {
+  langs=( "java" "c" "js" "pl" "py" )
+  for y in "${langs[@]}"; do
+    if [ -d ./${y} ]; then
+      count=$(ls -1 ./${y} 2>/dev/null | wc -l)
+      if [[ count -gt 0 ]]; then
+        tar -czf ${releasedir}/zipped/${1}-${y}.tar.gz ./${y}/*
+      fi
+    fi
+  done
+}
 
 # Make sure this wasn't run as root or with sudo.
 if
@@ -24,113 +59,56 @@ fi
 # What release are we scanning?
 echo "What release will be scanned?"
 read release
-
-# Source, temp, and target directories
-sources="/Users/stingley/Documents/Source/${release}/RPMs/"
-uncompressed="/Users/stingley/Documents/Source/${release}/uncompressed/"
-target="/Users/stingley/Documents/Source/${release}/jars/"
+user=$(whoami)
+# A Note On Directory Structure
+# This script assumes a particular directory structure to do its work
+# and sort the code correctly for upload.
+# By way of example, the following is a valid path example:
+# /Users/bob/Documents/Source/Arlington/platform/authentication-38_0_6
+# where, parameterized, the code sees it as:
+# /Users/$(whoami)/Documents/Source/${release}/${component}/${service}
+# and each service directory contains the RPMs that are part of that release
+releasedir="/Users/${user}/Documents/Source/${release}"
 
 # Ensure directory structure exists and RPMs are found
-if [ ! -d $sources ]; then
+if [ ! -d $releasedir ]; then
     echo "Could not find RPMs directory!"
     exit 11
-else
-    sudo chown -R stingley $sources
-    sudo chmod -R 775 $sources
-    cd $sources
-    count=$(ls -1 *.rpm 2>/dev/null | wc -l)
-    if [[ $count -gt 0 ]]; then
-        echo "RPMs appear to exist."
-        if [ ! -d $uncompressed ]; then
-            mkdir $uncompressed
-        fi
-        if [ ! -d $target ]; then
-            mkdir $target
-        fi
-        if [ ! -d ${target}java/ ]; then
-            mkdir ${target}java/
-        fi
-        if [ ! -d ${target}c/ ]; then
-            mkdir ${target}c/
-        fi
-        if [ ! -d ${target}js/ ]; then
-            mkdir ${target}js/
-        fi
-        if [ ! -d ${target}perl/ ]; then
-            mkdir ${target}perl/
-        fi
-        if [ ! -d ${target}python/ ]; then
-            mkdir ${target}python/
-        fi
-    else
-        echo "Could not find RPMs!"
-        exit 12
-    fi
 fi
 
-# Unpack the RPMs
-cd ${sources}
-for i in $(ls $sources); do
-    tar -xzf $i -C ${uncompressed}
+sudo chown -R ${user} $releasedir
+sudo chmod -R 775 $releasedir
+
+cd $releasedir
+if [ ! -d ${releasedir}/zipped ]; then
+  mkdir zipped
+else
+  rm -rf ${releasedir}/zipped/*
+fi
+for component in $(ls -d *); do
+  cd ${releasedir}/${component}
+  for service in $(ls -d *); do
+    cd ${releasedir}/${component}/${service}
+    subdirs=( "${service}-bin" "java" "c" "js" "pl" "py" )
+    for i in "${subdirs[@]}"; do
+      if [ ! -d ${releasedir}/${component}/${service}/${i} ]; then
+        mkdir ${releasedir}/${component}/${service}/${i}
+      else
+        rm -rf ${releasedir}/${component}/${service}/${i}/*
+      fi
+    done
+    for package in $(find . -type f -name "*.rpm"); do
+# If we wind up needing to go one more level of granularity, this is the code
+# to do it with.
+#        name=$(echo $package | sed -e s/\.el6.x86_64.rpm//g -e s/\.x86_64.rpm//g -e s/\.noarch.rpm//g -e s/\-centos7.rpm//g -e s/\.rpm//g)
+#        mkdir ./${serivice}-${name}-bin
+      tar -xzf $package -C ./${service}-bin/
+    done
+    sudo chown -R $user ./${service}-bin && sudo chmod -R 775 ./${service}-bin
+    sortcode ${service}-bin
+#    repackcode ${service}
+  done
 done
-
-echo "RPMs unpacked"
-
-sudo chown -R stingley $uncompressed
-sudo chmod -R 775 $uncompressed
-cd $uncompressed
-
-# File types supported by Veracode
-c=$(find . -type f \( -name "*.a" -o -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.so" -o -name "*.la" \))
-jars=$(find . -type f -name "*.jar")
-js=$(find . -type f -name "*.js")
-pl=$(find . -type f \( -name "*.pl" -o -name "*.pm" -o -name "*.plx" -o -name "*.pl5" -o -name "*.cgi" \))
-pysource=$(find . -type f -name "*.py")
-pyall=$(find . -type f \( -name "*.py" -o -name "*.htm" -o -name "*.html" \))
-
-# Gather the source files 
-for x in $(ls $jars); do
-    cp $x ${target}java/
-done
-echo "Java code ready"
-
-for x in $c; do
-    cp $x ${target}c/
-done
-cd ${target}c/
-tar -czf c.tar.gz ./*
-echo "C and C++ code ready"
-
-cd ${uncompressed}
-for x in $js; do
-    cp $x ${target}js/
-done
-cd ${target}js/
-tar -czf js.tar.gz ./*
-echo "Javascript code ready"
-
-cd ${uncompressed}
-for x in $pl; do
-    cp $x ${target}perl/
-done
-cd ${target}perl/
-tar -czf pl.tar.gz ./*
-echo "Perl code ready"
-
-cd ${uncompressed}
-for x in $pysource; do
-    cp $x ${target}python/
-done
-cd ${target}python/
-tar -czf py.tar.gz ./*
-echo "Python code ready"
-
-# Open the target directory and delete the temp directory
-cd $target
-sudo chown -R stingley $uncompressed
-sudo chmod -R 775 $uncompressed
-# rm -rf $uncompressed
-open .
 
 # We made it!
 exit 0
